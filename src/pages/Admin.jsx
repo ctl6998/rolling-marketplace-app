e8b894d645d99354cbe5f0e89b90d18532ec456d
ctl6998 <116896239+ctl6998@users.noreply.github.com>
2023-04-15 02:07:00 +0700

    update token for user

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,9 +1,9 @@
 import { Amplify } from "aws-amplify";
 import Container from "@mui/material/Container";
-import { Authenticator } from "@aws-amplify/ui-react";
+import { Authenticator, Alert } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 
-import awsExports from '../aws-exports';
+import awsExports from "../aws-exports";
 Amplify.configure(awsExports);
 
 export default function Admin() {
@@ -16,6 +16,14 @@ export default function Admin() {
         {({ signOut, user }) => (
           <main>
             <h1>Hello {user.username}</h1>
+            <Alert style={{overflowX:"scroll"}} isDismissible={false} hasIcon={true} heading="User jwtToken">
+              {user.signInUserSession.accessToken.jwtToken}
+            </Alert>
+            {/* <button
+              onClick={console.log(user)}
+            >
+              Click Me
+            </button> */}
             <button onClick={signOut}>Sign out</button>
           </main>
         )}
